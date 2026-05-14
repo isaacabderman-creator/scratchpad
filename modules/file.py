c9bdfb3040e2350f2cbccb7c5422b2a905e91bca
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-05-14 18:02:35 +0300

    fix: remove mypy error
    chore: create workflow directory
    
    Create type_check.yml for mypy type checking
    
    Add GitHub Actions workflow for type checking with mypy.

--- modules/file.py
+++ modules/file.py
@@ -1,14 +1,15 @@
 from pathlib import Path
-import texteditor
+import texteditor  # type: ignore
 
 
 def edit_file(DIR: Path, file_name: str) -> str:
     path = Path(DIR) / file_name
+    no_changes: int
     try:
         with path.open() as file:
-            no_changes: int = len(file.readline())
+            no_changes = len(file.readline())
     except FileNotFoundError:
-        no_changes: int = 0
+        no_changes = 0
         with path.open("w") as file:
             file.write("Starting file\n")
 
